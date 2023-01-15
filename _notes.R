f548eba09b9f3d395304b9d1d97b4da9714c5de9
Nathan Brouwer <brouwern@gmail.com>
2023-01-14 22:53:19 -0500

    updates

---
+++ _notes.R
@@ -0,0 +1,95 @@
+NDVI3g_GIMMS: Downloading and Processing the NDVI3g (Normal Difference Vegetation Index) produced by GIMMS
+https://github.com/brouwern/NDVI3g_GIMMS
+
+
+https://github.com/brouwern/rainfall_CHIRPS
+
+https://github.com/brouwern/climate_data_compilation
+
+https://github.com/brouwern/global_climate_indices
+
+
+## gamm
+not very clean but its there!
+gamm_derivatives
+https://github.com/brouwern/gamm_derivatives
+
+##  MotulkyRCompanion
+not in bookdown?
+https://github.com/brouwern/MotulkyRCompanion
+
+## sparrowtelomeres
+contain mostly stuff for recitation
+may be mostly complete
+https://github.com/brouwern/sparrowtelomeres/tree/master/vignettes
+
+## lloyd2016
+https://github.com/brouwern/lloyd2016
+Replication and re-analysis of Lloyd et al. 2016 "Assessing conservation status of resident and migrant birds on Hispaniola with mist-netting" PeerJ 3:e1541 https://doi.org/10.7717/peerj.1541
+
+no package down ey
+doesnt seem to have most stuff
+
+
+## Get R Done! A travelers guide to the world of R
+ready for public viewing?
+  https://brouwern.github.io/getRdone/
+
+
+## blastats
+https://github.com/brouwern/blaststats/tree/master/vignettes
+no packagdown
+original stuff from class
+no functions
+
+## gamkapva: Replication of Gamka Mountain Nature Preserve zebra population viability analysis of Watson et al. (2005)
+https://github.com/brouwern/gamkapva
+
+no packagedown
+no vignettes
+
+## countpva
+
+##LSAavian
+https://github.com/brouwern/LSAavian
+nothing ever pushed?
+
+shroom
+Data analysis examples for cell and molecular biology using data from University of Pittsburgh's Neural Defects lab course
+
+mammalsmilkRA
+Re-analysis and extension of Skibiel et al Journal of Animal Ecology
+
+mammalsmilk
+Regression examples using data from Skibiel et al 2010. "The evolution of the nutrient composition of mammalian milks"
+
+
+
+
+## popcycles
+https://github.com/brouwern/popcycles
+popcycles: R implementation of methods by Hal Caswell and collaborators to analyze periodic matrix models
+status?
+doesnt have packagedown yet
+mostly just code for reproducing stuff - only has 4 functions
+
+
+## carnivorephylo
+https://github.com/brouwern/carnivorephylo
+Replication of carnivore phylogeny examples from Baum and Smith (2013) Tree Thinking chapters 7, 8, 9
+
+BIOSCI_1540_lectures
+empty
+
+avesdemazan
+out of date
+
+oceanicislands
+empty
+https://github.com/brouwern/oceanicislands
+
+
+
+Computational Biology for All!
+  6 posts
+  https://compbio4all.netlify.app/
